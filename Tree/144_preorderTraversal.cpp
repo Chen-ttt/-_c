ea29a5075e77f8dbac5acecfb228ea2bab55d159
Tong <1141425427@qq.com>
2022-06-26 15:14:12 +0100

    Tree Ex.144 Pre-order Traversal II

--- Tree/144_preorderTraversal.cpp
+++ Tree/144_preorderTraversal.cpp
@@ -3,6 +3,7 @@
 #include "Tree.h"
 using namespace std;
 
+// 递归
 void Travel(TreeNode* p, vector<int>& result){
     if(p!=nullptr){
         result.push_back(p->val);
@@ -13,6 +14,24 @@ void Travel(TreeNode* p, vector<int>& result){
 vector<int> preorderTraversal(TreeNode* root) {
     vector<int> result;
     Travel(root, result);
+    return result;
+}
+
+// 非递归
+vector<int> preorderTraversal(TreeNode* root) {
+    stack<TreeNode*> s;
+    TreeNode *p = root;
+    s.push(p);
+    vector<int> result;
+    if(root==NULL) return result; // 若为[],则返回[]
+
+    while(!s.empty()){
+        TreeNode *top = s.top(); // 返回top元素
+        s.pop(); // 在栈中删除top元素
+        result.push_back(top->val);
+        if(top->right!=nullptr) s.push(top->right); // 若有孩子则push,必须先push右孩子
+        if(top->left!=nullptr) s.push(top->left);
+    } // 当最后一个node出栈且该node无孩子结点需要push,则栈空,退出循环
     return result;
 }
 
